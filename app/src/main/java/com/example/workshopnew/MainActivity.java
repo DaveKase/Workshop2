ab5e27deeb33bf1717da0d37b51d0793f30dc189
taaviKase <taavikase@gmail.com>
2020-07-07 15:06:35 +0300

    Inital commit. Changed API level to 30.

---
+++ app/src/main/java/com/example/workshopnew/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.workshopnew;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMainButtonClick(View v) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TextViewerActivity.class);
+        startActivity(intent);
+    }
+}
